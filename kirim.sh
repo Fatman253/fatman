6afe539051a07c3baa36c912c9180ff7761fdeb1
Fatman253 <23manfaat@gmail.com>
2023-01-31 19:35:59 +0700

    Add existing file

--- kirim.sh
+++ kirim.sh
@@ -1,5 +1,6 @@
+it remote set-url origin https://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@github.com/Fatman253/fatman.git
+git pull https://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@github.com/Fatman253/fatman.git
 git add .
 git commit -m "Add existing file"
 git push -u origin main
-fatman253
-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
+
